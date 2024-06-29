585218915eca8b36c57b37704b17f8c88bbdacb5
Aryan Saraswat <aryan.saraswat@hotmail.co.uk>
2024-06-30 01:47:23 +0200

    add input logic

--- src/components/input-box/InputBox.tsx
+++ src/components/input-box/InputBox.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+import "./InputBox.css";
+
 function InputBox() {
-  return <input type="text"></input>;
+  const [inputValue, setInputValue] = useState<string>("");
+
+  function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
+    const enteredText = event.target.value;
+    setInputValue(enteredText.endsWith(" ") ? "" : enteredText);
+  }
+
+  return (
+    <form className="form">
+      <input type="text" value={inputValue} onChange={handleInput}></input>
+    </form>
+  );
 }
 
 export default InputBox;
