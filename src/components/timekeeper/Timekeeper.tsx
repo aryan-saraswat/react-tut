693d618dd0c0505e4a973ee596091f49b73a0861
Aryan Saraswat <aryan.saraswat@hotmail.co.uk>
2024-06-30 23:29:42 +0200

    timekeeper improvements

--- src/components/timekeeper/Timekeeper.tsx
+++ src/components/timekeeper/Timekeeper.tsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 
 function Timekeeper() {
-  const [time, setTime] = useState<number>(60);
+  const [time, setTime] = useState<number>(5);
 
   useEffect(() => {
-    const timeout: NodeJS.Timeout | undefined = undefined;
     if (time > 0) {
       const timeout = setTimeout(() => {
         setTime(time - 1);
       }, 1000);
+
+      return () => clearTimeout(timeout);
     }
-    return () => clearTimeout(timeout);
   }, [time]);
 
   return <p>{time}</p>;
