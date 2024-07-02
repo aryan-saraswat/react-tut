ed03e9abdba7a07ad1224301044cc0a63d596b0f
Aryan Saraswat <aryan.saraswat@hotmail.co.uk>
2024-07-02 14:45:46 +0200

    add styles to wrong and correct guesses

--- src/components/words-holder/WordsHolder.tsx
+++ src/components/words-holder/WordsHolder.tsx
@@ -6,12 +6,22 @@ interface WordsHolderProps {
 }
 
 function WordsHolder(props: WordsHolderProps) {
+  function wordStyle(word: WordEntry): string {
+    return word.guessed ? (word.guessedCorrectly ? "green" : "red") : "black";
+  }
+
   return (
     <div className="words">
       <div className="words-holder">
         {props.words.map((word, i) => {
           return (
-            <div key={i} className="word">
+            <div
+              key={i}
+              className="word"
+              style={{
+                color: wordStyle(word),
+              }}
+            >
               {word.word}
             </div>
           );
