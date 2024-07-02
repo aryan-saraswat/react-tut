87ae1904f2beb577e3e60c5a119432661b010891
Aryan Saraswat <aryan.saraswat@hotmail.co.uk>
2024-07-02 10:23:47 +0200

    make class component to functional component

--- src/components/game-area/GameArea.tsx
+++ src/components/game-area/GameArea.tsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import { WordEntry } from "../../types/types";
 import InputBox from "../input-box/InputBox";
 import Timekeeper from "../timekeeper/Timekeeper";
@@ -9,53 +9,48 @@ interface GameAreaProps {
   num: number;
 }
 
-interface GameAreaState {
-  words: WordEntry[];
-}
+function GameArea(props: GameAreaProps) {
+  const [words, setWords] = useState<WordEntry[]>([]);
+  const numberOfWords = props.num;
 
-class GameArea extends Component<GameAreaProps, GameAreaState> {
-  words: string[] = [];
-  constructor(props: GameAreaProps) {
-    super(props);
-    this.state = {
-      words: [],
+  useEffect(() => {
+    const fetchAndSetWords = async () => {
+      const fetchedWords = await fetchWords(numberOfWords);
+      if (fetchedWords) setWords(fetchedWords);
     };
-    console.log("from constructor");
+    fetchAndSetWords();
+  }, [numberOfWords]);
+
+  async function fetchWords(num: number): Promise<WordEntry[] | undefined> {
+    try {
+      const response = await fetch("http://localhost:8080/get-words", {
+        body: JSON.stringify({
+          numberOfWords: numberOfWords,
+        }),
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data: string[] = await response.json();
+      return data.map((word) => ({
+        guessed: false,
+        guessedCorrectly: false,
+        word: word,
+      }));
+    } catch (err) {
+      console.log(err);
+      return undefined;
+    }
   }
 
-  componentDidMount(): void {
-    fetch("http://localhost:8080/get-words", {
-      body: JSON.stringify({
-        numberOfWords: 5,
-      }),
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data: string[]) => {
-        let wordEntries: WordEntry[] = data.map((word) => {
-          return {
-            word: word,
-            guessed: false,
-            guessedCorrectly: false,
-          };
-        });
-        this.setState({ words: wordEntries });
-      })
-      .catch((err) => console.log(err));
-  }
-
-  render() {
-    return (
-      <div className="game-area">
-        <WordsHolder words={this.state.words}></WordsHolder>
-        <InputBox></InputBox>
-        <Timekeeper></Timekeeper>
-      </div>
-    );
-  }
+  return (
+    <div className="game-area">
+      <WordsHolder words={words}></WordsHolder>
+      <InputBox></InputBox>
+      <Timekeeper></Timekeeper>
+    </div>
+  );
 }
 
 export default GameArea;
